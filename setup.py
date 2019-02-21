2036339a2a1d3ce1161c440ccc0936b93bd6c80a
Yomguithereal <guillaumeplique@gmail.com>
2019-02-21 13:06:22 +0100

    Dropping python2 support

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='quenouille',
       author='Guillaume Plique',
       author_email='kropotkinepiotr@gmail.com',
       keywords='url',
-      python_requires='>=2.7',
+      python_requires='>=3.0',
       packages=find_packages(exclude=['test']),
       package_data={'docs': ['README.md']},
       zip_safe=True)
