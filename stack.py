ee4a87793c26075098aede92a489e1d4dc9e0441
Yomguithereal <guillaumeplique@gmail.com>
2019-03-18 11:59:18 +0100

    Adding ftest case

---
+++ stack.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# Quenouille Stack Overflow Testing
+# =============================================================================
+#
+# Reproducing issues related to recursion & stack overflow.
+#
+from quenouille import imap_unordered
+
+DATA = range(1000)
+
+def worker(i):
+    return i
+
+for i in imap_unordered(DATA, worker, 25, group=lambda x: 1, group_parallelism=1, group_throttle=0.1):
+    print(i)
