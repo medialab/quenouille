21d24cb4a1e7eb4a54f11fb7d472835b75df226d
Yomguithereal <guillaumeplique@gmail.com>
2019-10-01 17:49:56 +0200

    Adding ftest demonstrating fact that threads are not closed properly Related to #14

---
+++ ftest/too_many_threads.py
@@ -0,0 +1,10 @@
+import threading
+from quenouille import imap_unordered
+
+def worker(item):
+    pass
+
+for i in range(1_000):
+    print('Pass %i - (%i active threads)' % (i + 1, threading.active_count()))
+    for _ in imap_unordered(range(1_000), worker, 100):
+        pass
