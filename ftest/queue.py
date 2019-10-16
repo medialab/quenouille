307373b8754d4ee4ebeabfc3ac5c9d51109cab36
Yomguithereal <guillaumeplique@gmail.com>
2019-10-16 17:02:24 +0200

    Little test

--- ftest/queue.py
+++ ftest/queue.py
@@ -8,12 +8,13 @@ queue.put(2)
 queue.put(1)
 queue.put(3)
 
-def consume(iterator):
-    for _ in iterator:
-        pass
-
 def worker(payload):
-    time.sleep(payload)
-    print('Done waiting %i' % payload)
+    time.sleep(payload[1])
 
-consume(imap_unordered(iter_queue(queue), worker, 1))
+    return payload
+
+for i, t in imap_unordered(enumerate(iter_queue(queue)), worker, 1):
+    print('Done waiting %i' % t)
+
+    if i < 1:
+        queue.put(4)
