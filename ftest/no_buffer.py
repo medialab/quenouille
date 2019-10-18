02dc8ee4b7aec4c84a92c138b76c540f6adb1f8e
Yomguithereal <guillaumeplique@gmail.com>
2019-10-18 17:18:26 +0200

    Adding ftest.no_buffer. Related to #1

---
+++ ftest/no_buffer.py
@@ -0,0 +1,29 @@
+import time
+from quenouille import imap_unordered
+from operator import itemgetter
+
+TASKS = [
+    ('A', 1),
+    ('A', 1),
+    ('A', 1),
+    ('B', 1),
+    ('C', 2)
+]
+
+
+def worker(payload):
+    time.sleep(payload[1])
+
+    return payload
+
+iterator = imap_unordered(
+    TASKS,
+    worker,
+    2,
+    group=itemgetter(0),
+    group_buffer_size=0,
+    group_parallelism=1
+)
+
+for g, t in iterator:
+    print(g, t)
