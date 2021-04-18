e2d51cba9c1878ef3776514ecbfb9cf81b9adc09
Yomguithereal <guillaumeplique@gmail.com>
2021-04-18 22:13:33 +0200

    Adding ftest.breaks_ordered

---
+++ ftest/breaks_ordered.py
@@ -0,0 +1,20 @@
+import threading
+import time
+from quenouille import imap
+
+def print_active_threads(i=-1):
+    print('Pass %s - (%i active threads)' % (i + 1, threading.active_count()))
+
+def worker(item):
+    time.sleep(0.01)
+    return item
+
+for i in range(1_000):
+    print_active_threads(i)
+    for j in imap(range(1_000), worker, 100):
+        if j > 500:
+            break
+
+print_active_threads()
+time.sleep(2)
+print_active_threads()
