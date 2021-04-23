b634140e7bb8dec1cad5753fcc3fd0b1dafd9267
Yomguithereal <guillaumeplique@gmail.com>
2021-04-23 22:46:47 +0200

    Add ftest for throttling state Related to #41

---
+++ ftest/throttle_state.py
@@ -0,0 +1,17 @@
+import time
+from quenouille import ThreadPoolExecutor
+
+key = lambda x: 'SAME'
+
+
+def work(i):
+    time.sleep(0.01)
+    return i
+
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    for i in executor.imap(range(2), work, key=key, throttle=5):
+        print('done', i)
+
+    for i in executor.imap(range(2), work, key=key, throttle=5):
+        print('done', i)
