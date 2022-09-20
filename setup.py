7288a481b482a72816e387cee84f02fb40dbbf85
Yomguithereal <guillaumeplique@gmail.com>
2022-09-20 17:14:34 +0200

    Bump 1.4.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='quenouille',
-      version='1.4.2',
+      version='1.4.3',
       description='A library of multithreaded iterator workflows.',
       long_description=long_description,
       long_description_content_type='text/markdown',
